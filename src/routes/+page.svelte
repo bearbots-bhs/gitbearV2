1d9e910f70cb9d2d7831221ffc34d5825c3c0e2f
luke89joseph <luke89joseph@gmail.com>
2025-12-17 00:04:52 +0000

    Vibes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,21 @@
 <script>
-    import { slide } from 'svelte/transition';
+    import { fade, slide } from 'svelte/transition';
     import { onMount } from "svelte";
 
     let toggle = $state(false);
+    let loadMessage = $state("Putting those two braincells to work...")
 
     onMount(() => {
         setTimeout(() => {toggle = true}, 100);
     })
 </script>
+<style>
+    #titleSpan {
+        color: rgb(255, 174, 0);
+    }
+</style>
 
 {#if toggle}
-    <h1 transition:slide style="user-select: none;">ONBOT JAVA MANAGEMENT SYSTEM</h1>
+    <h1 transition:slide style="user-select: none;"><span id="titleSpan">BEARBOTS</span> ONBOT JAVA CODE ARCHIVE</h1>
+    <h3 style="margin-top: 100px;" transition:fade={{delay: 500}}>{loadMessage}</h3>
 {/if}
